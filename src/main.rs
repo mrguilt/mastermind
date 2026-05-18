636a78232814fc23d39b7c928044178f26af8d40
mrguilt <charles@mrguilt.com>
2026-05-18 18:13:33 -0400

    Set up input a guess.

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,41 @@
 // A Rust implementation of the game Mastermind.
 // Created by I. Charles Barilleaux
 // Created on 2026-05-18
+use std::io;
 
 const COLORS:[char;4]=['r','y','g','b']; //colors of the pegs
  
 fn main() {
-//    let mut guess:Vec<&char>;
-    let fullguess="b,g,y,r";
 
-    let mut guess:Vec<&str>=fullguess.split(',').collect(); 
-    println!("Hello, world!");
-    for element in COLORS {
-        println!("{} is {}",element,color_to_number(element.to_ascii_lowercase()));
-    }
-    let test='C';
-    println!("{} is {}",test, test.to_ascii_lowercase());
+    println!("Mastermind for Rust");
+    println!("========== === ====\n");
 
-    for element in guess {
-        println!("{} is {}",element,color_to_number(element.chars().next().unwrap()));
+    loop { 
+//        let mut fullguess=String::new();
+        let mut guess=String::new();
+
+        println!("Enter your guess: ");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let mut fullguess=guess.trim();
+        if fullguess.trim() == "quit" {
+            break;
+        }
+
+        let mut guess:Vec<&str>=fullguess.split(',').collect(); 
+//        println!("length: {}",guess.len());
+
+        if guess.len()!=4 {
+            println!("INCORRECT GUESS FORMAT!!! Please enter 4 \"pegs\" (p,p,p,p).");
+        } else {
+            for element in guess {
+                println!("{} is {}",element,color_to_number(element.chars().next().unwrap()));
+            }
+        }
     }
+
 }
 
 //Returns the array index matching to the peg color. 
